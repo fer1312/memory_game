5a97afca16a5ffda3d2f1d3f561fa2a4ad2902f5
fer1312 <fer.dorantes13312@gmail.com>
2025-04-28 13:39:41 -0600

    Archivo de pruebas unitarias

---
+++ test_jueguito.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 28 13:18:32 2025
+
+@author: mxfer
+"""
+
+import unittest
+from pygame import image
+from jueguito import Carta
+
+# Definir una clase de prueba
+class TestCarta(unittest.TestCase):
+
+    def test_voltear(self):
+        # Crear una carta con una imagen de prueba
+        carta = Carta(1, image.load('sandia.png'), (0, 0))
+        
+        # Comprobar que inicialmente no está volteada
+        self.assertFalse(carta.esta_volteada)
+        
+        # Voltear la carta
+        carta.voltear()
+        
+        # Comprobar que ahora está volteada
+        self.assertTrue(carta.esta_volteada)
+
+    def test_es_par(self):
+        # Crear dos cartas con el mismo id
+        carta1 = Carta(1, image.load('sandia.png'), (0, 0))
+        carta2 = Carta(1, image.load('sandia.png'), (100, 100))
+        
+        # Comprobar si son iguales (par)
+        self.assertTrue(carta1.es_par(carta2))
+        
+        # Crear una carta diferente
+        carta3 = Carta(2, image.load('platano.png'), (200, 200))
+        
+        # Comprobar si no son iguales
+        self.assertFalse(carta1.es_par(carta3))
+
+if __name__ == '__main__':
+    unittest.main()
